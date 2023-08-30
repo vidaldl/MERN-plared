87a98ee4622bb968c47f35877e19ed0de1619d47
Leo Vidal <leo@MacBook-Pro-de-Leo.local>
2023-08-30 12:19:35 -0600

    frontend - Added validation to inputs

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Input from "../../shared/components/FormElments/Input";
 
+import {VALIDATOR_REQUIRE} from "../../shared/util/validator";
 import './NewPlace.css';
 const NewPlace = () => {
     return (
         <form className="place-form">
-            <Input type="text" label="Title" element="input" />
+            <Input
+                type="text"
+                label="Title"
+                element="input"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title"
+            />
         </form>
     );
 };
